90bbf997857845dc2becb9ad9ca3dcd5a60f8110
Kenny F <kfrachelle123@gmail.com>
2020-09-16 18:57:30 +0800

    docs(ast): improved AST specs

--- src/ast/expressions/property-name.ts
+++ src/ast/expressions/property-name.ts
@@ -1,20 +1,23 @@
+import { IdentifierReference } from './identifierreference';
 import { Node } from '../node';
-import { PropertyKey } from '.';
+import { Expression, PropertyKey } from '.';
 import { AssignmentExpression } from './assignment-expr';
 import { BindingElement } from './binding-element';
+import { BindingIdentifier } from './binding-identifier';
 import { AssignmentElement } from './assignment-element';
+import { IdentifierName } from './identifiername';
 
 /**
  * Property name
  */
 export interface PropertyName extends Node {
-  readonly key: PropertyKey;
-  readonly value: AssignmentExpression | BindingElement | AssignmentElement;
+  readonly key: PropertyKey | IdentifierReference | IdentifierName;
+  readonly value: AssignmentExpression | BindingElement | AssignmentElement | BindingIdentifier | Expression;
 }
 
 export function createPropertyName(
   key: PropertyKey,
-  value: AssignmentExpression | BindingElement | AssignmentElement
+  value: AssignmentExpression | BindingElement | AssignmentElement | BindingIdentifier
 ): PropertyName {
   return {
     type: 'PropertyName',
